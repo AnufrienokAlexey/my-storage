2adb558aca01c2860c51db9ea95db5924227b07e
Alexey Anufrienok <alex.anufrienok@gmail.com>
2023-03-11 02:42:46 +0300

    user.php

--- public/includes/sign-in.php
+++ public/includes/sign-in.php
@@ -3,8 +3,12 @@ session_start();
 
 require __DIR__ . '/../../vendor/autoload.php';
 
+var_dump($_POST);
+
 if (($_POST['login'] !== null) AND ($_POST['password']) !== null) {
+	echo 'sign-in';
 	$user = new User();
 	$_SESSION['user'] = $user->selectUserFromDatabase($_POST['login'], $_POST['password']);
-	header('Location: ../views/lk.php');
+	var_dump($_SESSION['user']);
+//	header('Location: ../views/lk.php');
 }
--- src/User.php
+++ src/User.php
@@ -4,16 +4,18 @@ class User {
 
 	public function selectUserFromDatabase($login, $password)
 	{
+		$arr = [];
 		try {
 			$connect = new PDO("mysql:host=localhost;dbname=my-storage;charset=utf8", "root", "");
 			$password = md5($password);
 			$users = $connect->prepare("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
 			$users->execute();
-			return $users->fetchAll(PDO::FETCH_ASSOC);
+			$arr = $users->fetchAll(PDO::FETCH_ASSOC);
 		}
 		catch(PDOException $e) {
 			echo "Неудачная попытка подключения к базе данных: " . $e->getMessage();
 		}
+		return $arr;
 	}
 
 	public function addNewUserToDatabase($login, $email, $password, $fullName)
